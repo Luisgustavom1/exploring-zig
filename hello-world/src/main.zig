dced8570fe91a8621efa27cfc457dbed004ff9fc
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-09-16 23:28:48 -0300

    feat: type values

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -1,19 +1,48 @@
 const std = @import("std");
 
+const print = std.debug.print;
+const assert = std.debug.assert;
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+   const stdout = std.io.getStdOut().writer();
+    try stdout.print("Hello, {s}\n", .{"world"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    std.debug.print("shorthand of std.io.getStdOut().writer()\n\n", .{});
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // integer
+    const integer32: i32 = 1 + 1;
+    print("1 + 1 = {}\n", .{integer32});
 
-    try bw.flush(); // don't forget to flush!
+    // float 
+    const myFloat: f32 = 6.0 / 3.0;
+    print("6.0 / 3.0 = {}\n", .{myFloat});
+
+    // boolean
+    print("and -> {}\n", .{true and false});
+    print("or -> {}\n", .{true or false});
+    print("! -> {}\n", .{!true});
+
+    // optional value
+    var my_optional: ?[]const u8 = null;
+    assert(my_optional == null);
+
+    print("my optional type: {}\nvalue: {?s}\n", .{
+        @TypeOf(my_optional), my_optional,
+    });
+
+    my_optional = "oi";
+    assert(my_optional != null);
+
+    print("my optional type: {}\nvalue: {?s}\n", .{
+        @TypeOf(my_optional), my_optional,
+    });
+
+    // error union
+    var number_or_error: anyerror!i32 = error.ArgNotFound;
+
+    print("my error union\ntype: {}\nvalue: {!}\n", .{
+        @TypeOf(number_or_error), number_or_error,
+    });
 }
 
 test "simple test" {
